164a7423516bbf6b8a9a7f471d0ca97972f03879
akil.harris <akil.harris@chillchillchillchillchillchill.com>
2013-10-28 21:51:15 -0400

    stub for url logging function.

--- restaurant-server.js
+++ restaurant-server.js
@@ -16,15 +16,16 @@ var server = express.createServer();
 server.get('/getRestaurantGrade', function (request, response) {
     var _get = url.parse(request.url, true);
     var query = _get.query;
-
+    console.log(request.headers);
     console.log('getRestaurantGrade for ' + query.name);
 
     var restaurantData = {
-        name: (query.name) ? query.name : '',
-        zip_code: (query.zip_code) ? parseInt(query.zip_code) : 0,
-        street_name: (query.street_name) ? query.street_name : '',
-        building: (query.building) ? query.building : '',
-        telephone: (query.telephone) ? query.telephone : ''
+        name        : (query.name) ? query.name : '',
+        zip_code    : (query.zip_code) ? parseInt(query.zip_code) : 0,
+        street_name : (query.street_name) ? query.street_name : '',
+        building    : (query.building) ? query.building : '',
+        telephone   : (query.telephone) ? query.telephone : '',
+        URL         : 'test'
     };
 
     mongoConnect.fetchRestaurant(restaurantData, function (responseBody) {
@@ -39,10 +40,10 @@ var buildResponse = function(results) {
     if(results !== -1) {
 
         var responseData = {
-            grade_image: results.grade_image,
-            last_inspection_date: results.last_inspected_date,
-            current_grade: results.current_grade,
-            score_violations: results.score_violations
+            grade_image          : results.grade_image,
+            last_inspection_date : results.last_inspected_date,
+            current_grade        : results.current_grade,
+            score_violations     : results.score_violations
         };
 
         return responseData;
