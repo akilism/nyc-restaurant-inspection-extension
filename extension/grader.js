221fe8d495e21ee7007203a6c4d24592f8918ec4
akil.harris <akil.harris@chillchillchillchillchillchill.com>
2013-11-05 22:46:24 -0500

    try to clean the name up a little and try one last search.

--- extension/grader.js
+++ extension/grader.js
@@ -40,8 +40,10 @@ var restaurantGrader = {
         }
 
         if (restaurantData) {
+            var zips = [10000, 10001, 10002, 10003, 10004, 10005, 10006, 10007, 10009, 10010, 10011, 10012, 10013, 10014, 10016, 10017, 10018, 10019, 10020, 10021, 10022, 10023, 10024, 10025, 10026, 10027, 10028, 10029, 10030, 10031, 10032, 10033, 10034, 10035, 10036, 10037, 10038, 10039, 10040, 10044, 10055, 10057, 10065, 10069, 10075, 10104, 10105, 10106, 10110, 10111, 10112, 10115, 10118, 10119, 10121, 10123, 10128, 10153, 10154, 10168, 10170, 10171, 10174, 10175, 10176, 10179, 10199, 10270, 10280, 10281, 10282, 10285, 10301, 10302, 10303, 10304, 10305, 10306, 10307, 10308, 10309, 10310, 10311, 10312, 10314, 10451, 10452, 10453, 10454, 10455, 10456, 10457, 10458, 10459, 10460, 10461, 10462, 10463, 10464, 10465, 10466, 10467, 10468, 10469, 10470, 10471, 10472, 10473, 10474, 10475, 11001, 11004, 11005, 11040, 11101, 11102, 11103, 11104, 11105, 11106, 11109, 11201, 11203, 11204, 11205, 11206, 11207, 11208, 11209, 11210, 11211, 11212, 11213, 11214, 11215, 11216, 11217, 11218, 11219, 11220, 11221, 11222, 11223, 11224, 11225, 11226, 11228, 11229, 11230, 11231, 11232, 11233, 11234, 11235, 11236, 11237, 11238, 11239, 11242, 11249, 11256, 11354, 11355, 11356, 11357, 11358, 11360, 11361, 11362, 11363, 11364, 11365, 11366, 11367, 11368, 11369, 11370, 11371, 11372, 11373, 11374, 11375, 11377, 11378, 11379, 11385, 11411, 11412, 11413, 11414, 11415, 11416, 11417, 11418, 11419, 11420, 11421, 11422, 11423, 11426, 11427, 11428, 11429, 11430, 11432, 11433, 11434, 11435, 11436, 11451, 11691, 11692, 11693, 11694, 11697];
+            if (zips.indexOf(parseInt(restaurantData.zip_code, 10)) === -1) { return null; }
+
             restaurantData.name = this.cleanName(restaurantData.name);
-//            console.log("https://pure-depths-9510.herokuapp.com/getRestaurantGrade?name=" + restaurantData.name + "&zip_code=" + restaurantData.zip_code + "&street_name=" + restaurantData.street_name + "&building=" + restaurantData.building + "&telephone=" + restaurantData.telephone);
             return "https://pure-depths-9510.herokuapp.com/getRestaurantGrade?name=" + restaurantData.name + "&zip_code=" + restaurantData.zip_code + "&street_name=" + restaurantData.street_name + "&building=" + restaurantData.building + "&telephone=" + restaurantData.telephone;
         }
 
@@ -322,7 +324,9 @@ var restaurantGrader = {
         $e.addClass('animate');
     },
     inValidURL: function() {
-        var url = this.fetchURL().toLowerCase();
+        var url = this.fetchURL();
+
+        if (url) { url = url.toLowerCase() } else { return false; }
 
         if ((url.indexOf('/members/') !== -1) ||
             (url.indexOf('account.') !== -1) ||
@@ -339,13 +343,13 @@ var restaurantGrader = {
             return;
         }
 
-        this.setLoader();
-
-        var request = new XMLHttpRequest();
         var url = this.fetchURL();
 
         if (url == null) { return; }
 
+        this.setLoader();
+
+        var request = new XMLHttpRequest();
         request.open("GET", url, true);
         request.onload = this.setInspectionData.bind(this);
         request.send(null);
@@ -414,19 +418,6 @@ var restaurantGrader = {
                 $(".inspection-holder").append($results);
                 $results.fadeIn(125);
             });
-//            if(url.toLowerCase().indexOf('yelp') != -1) {
-//                $('#bizPhone').after('<div class="inspection-holder ' + holder_class + '"><img class="inspection-grade" src="' + grade_details.grade_image + '">' + date_details + '</div>');
-//            } else if (url.toLowerCase().indexOf('seamless') != -1){
-//                $('.consumer').after('<div class="inspection-holder ' + holder_class + '"><img class="inspection-grade" src="' + grade_details.grade_image + '">' + date_details + '</div>');
-//            } else if (url.toLowerCase().indexOf('zagat') != -1){
-//                $('.date').after('<div class="inspection-holder ' + holder_class + '"><img class="inspection-grade" src="' + grade_details.grade_image + '">' + date_details + '</div>');
-//            } else if (url.toLowerCase().indexOf('grubhub') != -1){
-//                $('h3').after('<div class="inspection-holder ' + holder_class + '"><img class="inspection-grade" src="' + grade_details.grade_image + '">' + date_details + '</div>');
-//            } else if (url.toLowerCase().indexOf('menupages') != -1){
-//                $('h1').after('<div class="inspection-holder ' + holder_class + '"><img class="inspection-grade" src="' + grade_details.grade_image + '">' + date_details + '</div>');
-//            } else if (url.toLowerCase().indexOf('delivery') != -1){
-//                $('.top_name').after('<div style="float: none;" class="inspection-holder ' + holder_class + '"><img class="inspection-grade" src="' + grade_details.grade_image + '">' + date_details + '</div>');
-//            }
         }
     }
 };
